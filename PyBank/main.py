2185268e4f37527a685b3c7f2cfd511c57d71d04
dxvo <dxvo@uh.edu>
2018-12-02 21:42:27 -0600

    update PyBank script

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,29 +10,43 @@ with open(file, newline = '') as inputFile:
 		total = 0
 		greatest_increase = 0
 		greatest_decrease = 0
-		arr = ["",""]
+		arr = ["",""] #result[0] for greast_increase
+		data = []
+		monthly = []
+		total_change = 0
+
 
 		for row in file_obj:
 			total += int(row[1])
 			month += 1
-			curr_value = int(row[1])
+			data.append(row)
 
-			if(curr_value > greatest_increase):
-				greatest_increase = curr_value
-				arr[0] = row
-			elif (curr_value  < greatest_decrease):
-				greatest_decrease = curr_value
-				arr[1] = row
+
+
+		for i in range(1,len(data)):
+			change= int(data[i][1]) - int(data[i-1][1])
+			monthly.append(change)
+			total_change += change
+
+
+			if(change > greatest_increase):
+				greatest_increase = change
+				arr[0] = data[i][0]
+			elif (change  < greatest_decrease):
+				greatest_decrease = change
+				arr[1] = data[i][0]
+
+		avg_change =round(total_change/(month - 1),2)
 				
 	finally:
 		inputFile.close()
-
+print()
 print("\n---------FINANCIAL ANALYSIS---------")
 print("Total month: ",month)
 print("Total: ","$"+ str(total))
-print("Avg Change: ","$" + str(round(total/month,2)))
-print("Greatest Increase in Profits: ",arr[0][0],"($" + str(arr[0][1])+ ")")
-print("Greatest Decrease in Profits: ",arr[1][0],"($" + str(arr[1][1])+ ")")
+print("Avg Change: ","$" + str(avg_change))
+print("Greatest Increase in Profits: ",arr[0],"($" + str(greatest_increase)+ ")")
+print("Greatest Decrease in Profits: ",arr[1],"($" + str(greatest_decrease)+ ")")
 
 
 with open("PyBank_out.txt",'w') as outFile:
@@ -40,9 +54,9 @@ with open("PyBank_out.txt",'w') as outFile:
 		outFile.write("\n---------FINANCIAL ANALYSIS---------\n")
 		outFile.write("Total month: " + str(month)+ "\n")
 		outFile.write("Total: "+ "$"+ str(total)+"\n")
-		outFile.write("Avg Change: "+"$" + str(round(total/month,2))+"\n")
-		outFile.write("Greatest Increase in Profits: " + str(arr[0][0])+" " +"($" + str(arr[0][1])+ ")"+"\n")
-		outFile.write("Greatest Decrease in Profits: " + str(arr[1][0])+ " " + "($" + str(arr[1][1])+ ")"+"\n")
+		outFile.write("Avg Change: "+"$" + str(avg_change)+"\n")
+		outFile.write("Greatest Increase in Profits: " + str(arr[0])+" " +"($" + str(greatest_increase)+ ")"+"\n")
+		outFile.write("Greatest Decrease in Profits: " + str(arr[1])+ " " + "($" + str(greatest_decrease)+ ")"+"\n")
 	finally:
 		outFile.close()
 
