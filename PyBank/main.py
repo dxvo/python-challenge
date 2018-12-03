91abd7798f4793c6d35a68994e9293e08a38e544
dxvo <dxvo@uh.edu>
2018-12-02 21:58:59 -0600

    PyBank Final

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,9 +10,8 @@ with open(file, newline = '') as inputFile:
 		total = 0
 		greatest_increase = 0
 		greatest_decrease = 0
-		arr = ["",""] #result[0] for greast_increase
+		result_date = ["",""] #result[0] for greast_increase
 		data = []
-		monthly = []
 		total_change = 0
 
 
@@ -25,18 +24,17 @@ with open(file, newline = '') as inputFile:
 
 		for i in range(1,len(data)):
 			change= int(data[i][1]) - int(data[i-1][1])
-			monthly.append(change)
 			total_change += change
 
-
+			#find largest increase and decrease 
 			if(change > greatest_increase):
 				greatest_increase = change
-				arr[0] = data[i][0]
+				result_date[0] = data[i][0]
 			elif (change  < greatest_decrease):
 				greatest_decrease = change
-				arr[1] = data[i][0]
+				result_date[1] = data[i][0]
 
-		avg_change =round(total_change/(month - 1),2)
+		avg_change =round(total_change/(month - 1),2) 
 				
 	finally:
 		inputFile.close()
@@ -45,8 +43,8 @@ print("\n---------FINANCIAL ANALYSIS---------")
 print("Total month: ",month)
 print("Total: ","$"+ str(total))
 print("Avg Change: ","$" + str(avg_change))
-print("Greatest Increase in Profits: ",arr[0],"($" + str(greatest_increase)+ ")")
-print("Greatest Decrease in Profits: ",arr[1],"($" + str(greatest_decrease)+ ")")
+print("Greatest Increase in Profits: ",result_date[0],"($" + str(greatest_increase)+ ")")
+print("Greatest Decrease in Profits: ",result_date[1],"($" + str(greatest_decrease)+ ")")
 
 
 with open("PyBank_out.txt",'w') as outFile:
@@ -55,8 +53,8 @@ with open("PyBank_out.txt",'w') as outFile:
 		outFile.write("Total month: " + str(month)+ "\n")
 		outFile.write("Total: "+ "$"+ str(total)+"\n")
 		outFile.write("Avg Change: "+"$" + str(avg_change)+"\n")
-		outFile.write("Greatest Increase in Profits: " + str(arr[0])+" " +"($" + str(greatest_increase)+ ")"+"\n")
-		outFile.write("Greatest Decrease in Profits: " + str(arr[1])+ " " + "($" + str(greatest_decrease)+ ")"+"\n")
+		outFile.write("Greatest Increase in Profits: " + result_date[0]+" " +"($" + str(greatest_increase)+ ")"+"\n")
+		outFile.write("Greatest Decrease in Profits: " + result_date[1]+ " " + "($" + str(greatest_decrease)+ ")"+"\n")
 	finally:
 		outFile.close()
 
